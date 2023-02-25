62cf9501145b30a31ba62af5be8efbae48813877
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-02-25 22:01:14 +0530

    use JS to populate cells in rows and populate rows in cell container with proper html structure and attributes for css

--- script.js
+++ script.js
@@ -17,4 +17,10 @@ for(let i = 1; i <= 100; i++){
 }
 
 
-
+for(let i = 1; i <= 100; i++){
+    let row = $(`<div class="cell-row"></div>`);
+    for(let j = 1; j <= 100; j++){
+        row.append(`<div id="row-${i}-col-${j}" class="input-cell" contenteditable="true"></div>`);
+    }
+    $("#cells").append(row);
+  }
