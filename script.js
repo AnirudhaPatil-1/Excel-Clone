88dbacb3c81a3469ff03df070377916fb672b441
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-02-26 21:49:02 +0530

    scroll index row and index columns along with the scroll bar & make the cells editable

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const ps = new PerfectScrollbar("#cells",{
+    wheelSpeed: 15,
+    wheelPropagation: true,
+});
+
+
 for(let i = 1; i <= 100; i++){
     let str = "";
     let n = i;
@@ -20,7 +26,45 @@ for(let i = 1; i <= 100; i++){
 for(let i = 1; i <= 100; i++){
     let row = $(`<div class="cell-row"></div>`);
     for(let j = 1; j <= 100; j++){
-        row.append(`<div id="row-${i}-col-${j}" class="input-cell" contenteditable="true"></div>`);
+        row.append(`<div id="row-${i}-col-${j}" class="input-cell" contenteditable="false"></div>`);
     }
     $("#cells").append(row);
-  }
+}
+
+// $("#cells").scroll((e) => {
+//     $("#columns").scrollLeft(this.scrollLeft);
+//     $("#rows").scrollTop(this.scrollTop);
+// })
+
+$("#cells").scroll(function(e){
+    $("#columns").scrollLeft(this.scrollLeft);
+    $("#rows").scrollTop(this.scrollTop);
+})
+
+$(".input-cell").dblclick(function(e){
+    $(this).attr("contenteditable", "true");
+    $(this).focus();
+});
+
+$(".input-cell").blur(function(e){
+    $(this).attr("contenteditable","false");
+});
+
+$(".input-cell").click(function(e){
+    // $(".input-cell.selected").removeClass("selected");
+    // $(this).addClass("selected");
+    selectedCell(this, e);
+});
+
+function selectedCell(ele, e, topCell, bottomCell, leftCell, rightCell){
+    if(e.ctrlKey){
+        let topSelected;
+        if(topCell){
+            topSelected = topCell.hasClass("selected");
+        }
+
+    }else{
+        $(".input-cell.selected").removeClass("selected");
+    }
+    $(ele).addClass("selected");
+}
